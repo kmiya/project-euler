6165d9a60c6b1bd1af961bc243562e8aaffa110f
kmiya <kmiya@outlook.com>
2013-04-26 10:43:13 +0900

    simplified p18 and p67

--- 01-19/p18.cc
+++ 01-19/p18.cc
@@ -1,37 +1,34 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-const int gLine = 15;
-
 int main() {
-  const int max_num = gLine*(gLine + 1) / 2;
-  vector<int> mat;
+  const int line = 15;
+  const int max_num = line*(line + 1) / 2;
+  int mat[max_num];
   int n = 0;
   for (auto i = 0; i < max_num; i++) {
     cin >> n;
-    mat.push_back( n );
+    mat[i] = n;
   }
   int ret[max_num];
   for (auto i = 0; i < max_num; i++) { ret[i] = 0; } // initialize ret
   ret[0] = mat[0];
   int i = 0, row = 1, col = 1;
-  for (; i < max_num - gLine; i++) {
+  for (; i < max_num - line; i++) {
     for (auto k = 0; k < 2; k++) {
       int sum = ret[i] + mat[i + row + k];
       if (ret[i + row + k] < sum) {
         ret[i + row + k] = sum;
       }
     }
-    if (row == col) {
+    col++;
+    if (row < col) {
       row++;
       col = 1;
-    } else {
-      col++;
     }
   }
   int ans = 0;
-  for (; i < gLine*(gLine + 1) / 2; i++) {
+  for (; i < line*(line + 1) / 2; i++) {
     if (ret[i] > ans) {
       ans = ret[i];
     }
