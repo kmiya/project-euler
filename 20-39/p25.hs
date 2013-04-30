a41c1cc6bce6d6f1ea1ee7db5194eb5900feb32f
kmiya <kmiya@outlook.com>
2013-04-30 15:47:30 +0900

    added type signatures

--- 20-39/p25.hs
+++ 20-39/p25.hs
@@ -1,5 +1,8 @@
+fib :: Int -> Int
 fib n = fibs !! n where fibs = 0:1:zipWith (+) fibs (tail fibs)
 
-p25 n = if 1000 <= (length $ show $ (fib n)) then n else p25 (n+1)
+p25 :: Int -> Int
+p25 n = if 1000 <= (length $ show $ fib n) then n else p25 (n+1)
 
+main :: IO ()
 main = print $ p25 1
